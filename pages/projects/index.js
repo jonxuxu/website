e85e394eb0b2034b09059c25c8c65a924ef86a40
Jonathan <jonathanxu25@gmail.com>
2021-01-07 18:13:18 -0800

    new project links

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,7 +1,28 @@
 import Head from "next/head";
-import { Typography, List, Space } from "antd";
+import Image from "next/image";
+import Link from "next/link";
+import styled from "styled-components";
+import { Typography, List, Space, Row, Col } from "antd";
 
 const { Title } = Typography;
+
+const LinkCover = styled.a`
+  display: block;
+  margin-bottom: 20px;
+  overflow: hidden;
+  border-radius: 2px;
+  max-height: 360px;
+`;
+
+const CoverImg = styled.img`
+  margin: 0;
+  transform: scale(1);
+  transition: 0.3s transform;
+  max-width: 100%;
+  &:hover {
+    transform: scale(1.05);
+  }
+`;
 
 const ProjectsPage = () => {
   return (
@@ -11,9 +32,32 @@ const ProjectsPage = () => {
       </Head>
       <Title>Projects</Title>
       <p>
-        Some things that I built, worked on, and experimented with. This is a
-        work in progress.
+        This page is a work in progress, please mind the dust as I document the
+        things that I have built, worked on, and experimented with.
       </p>
+      <Row gutter={20}>
+        <Col xs={24} sm={12}>
+          {/* <Link href="/projects/noisy" passHref>
+            <LinkCover>
+              <CoverImg alt="noisy" src="/images/projects/noisy/banner.png" />
+            </LinkCover>
+          </Link> */}
+          <LinkCover href="https://noisy.live" target="_blank">
+            <CoverImg alt="noisy" src="/images/projects/noisy/banner.png" />
+          </LinkCover>
+          <LinkCover
+            href="https://www.hackster.io/jonathan-xu/brett-the-fire-mapping-drone-94fc1c"
+            target="_blank"
+          >
+            <CoverImg alt="brett" src="/images/projects/brett/banner.png" />
+          </LinkCover>
+        </Col>
+        <Col xs={24} sm={12}>
+          <LinkCover href="https://trofi.app" target="_blank">
+            <CoverImg alt="trofi" src="/images/projects/trofi/banner.png" />
+          </LinkCover>
+        </Col>
+      </Row>
     </div>
   );
 };
