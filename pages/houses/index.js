98614908d3c1cd24d145a41cb9b9dcf587f0b42f
Jonathan <jonathanxu25@gmail.com>
2021-05-01 18:30:45 +0800

    publishing 2 posts

--- pages/houses/index.js
+++ pages/houses/index.js
@@ -11,16 +11,15 @@ const HousePage = () => {
     <div>
       <Title>Good Houses</Title>
       <p>
-        Down here I'm going to share the active houses I know of that
-        demonstrate authentic living quite well. Maybe you can check them out,
-        apply, or be inspired to start your own. Message me if you'd like to add
-        (yours or others) to this list!
+        Here's a list of the active houses I know of that I think foster great
+        communities. Maybe you can check them out, apply, or be inspired to
+        start your own. Message me if you'd like to add (yours or others) to
+        this list!
       </p>
       <p>
-        Please be sure to check out{" "}
-        <Link href="/blog/2021-04-28-group-houses">this post</Link> if you
-        haven't yet, these resources are most hepful if you are using them with
-        the right intent.
+        Do check out <Link href="/blog/2021-04-28-group-houses">this post</Link>{" "}
+        if you haven't yet, this resource is most hepful if you are using it
+        with the right intent.
       </p>
       <List
         itemLayout="vertical"
@@ -29,13 +28,7 @@ const HousePage = () => {
         renderItem={(item) => (
           <List.Item
             key={item.name}
-            extra={
-              <img
-                width={272}
-                alt="logo"
-                src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
-              />
-            }
+            extra={item.img && <img width={272} alt="logo" src={item.img} />}
           >
             <List.Item.Meta
               title={<a href={item.url}>{item.name}</a>}
