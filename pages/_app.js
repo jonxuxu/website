ad37a60e613e5bfc07dc97fbadd6cf8695cf0190
Jonathan <jonathanxu25@gmail.com>
2021-05-11 13:03:14 +0800

    adding fathom

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import { Typography, Image, Divider } from "antd";
 import { MDXProvider } from "@mdx-js/react";
+import * as Fathom from "fathom-client";
 
 import { Container } from "../components/layout";
 import { CodeBlock, BlockQuote } from "../components/mdx";
@@ -15,6 +18,30 @@ const components = {
 };
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Initialize Fathom when the app loads
+    // Example: yourdomain.com
+    //  - Do not include https://
+    //  - This must be an exact match of your domain.
+    //  - If you're using www. for your domain, make sure you include that here.
+    Fathom.load("EOGCMSMH", {
+      includedDomains: ["jonathanxu.com"],
+    });
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+    // Record a pageview when route changes
+    router.events.on("routeChangeComplete", onRouteChangeComplete);
+
+    // Unassign event listener
+    return () => {
+      router.events.off("routeChangeComplete", onRouteChangeComplete);
+    };
+  }, []);
+
   return (
     <div>
       <MDXProvider components={components}>
