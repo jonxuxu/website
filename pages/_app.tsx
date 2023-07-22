a048e064e0ed5c63c5cf14d0297aa989bb53b618
Jonathan <jonathanxu25@gmail.com>
2023-07-22 17:05:59 +0800

    nextjs 13, with antd, custom theme

---
+++ pages/_app.tsx
@@ -0,0 +1,11 @@
+import { ConfigProvider } from 'antd';
+import type { AppProps } from 'next/app';
+import theme from '../styles/themeConfig';
+
+const App = ({ Component, pageProps }: AppProps) => (
+  <ConfigProvider theme={theme}>
+    <Component {...pageProps} />
+  </ConfigProvider>
+);
+
+export default App;
