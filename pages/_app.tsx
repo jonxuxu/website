a4b99f4ee4d69f17136ce3221ac7d58b5590d9fa
Jonathan <jonathanxu25@gmail.com>
2023-07-22 17:07:57 +0800

    analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,16 @@
-import { ConfigProvider } from 'antd';
 import type { AppProps } from 'next/app';
+
+import { Analytics } from '@vercel/analytics/react';
+import { ConfigProvider } from 'antd';
 import theme from '../styles/themeConfig';
 
 const App = ({ Component, pageProps }: AppProps) => (
-  <ConfigProvider theme={theme}>
-    <Component {...pageProps} />
-  </ConfigProvider>
+  <>
+    <ConfigProvider theme={theme}>
+      <Component {...pageProps} />
+    </ConfigProvider>
+    <Analytics />
+  </>
 );
 
 export default App;
