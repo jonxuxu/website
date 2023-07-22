fd9c4aa9904fa417adfaaf8d371c6051e6c6dc8f
Jonathan <jonathanxu25@gmail.com>
2023-07-22 16:48:20 +0800

    clear empty

--- prisma/migrations/0_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Lesson" (
-    "id" SERIAL NOT NULL,
-    "createdAt" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "lesson" TEXT NOT NULL,
-
-    CONSTRAINT "Lesson_pkey" PRIMARY KEY ("id")
-);
-
